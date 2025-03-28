2b23847e4cfa8085d6fcba6950848a15a9d9759b
revike <revike@ya.ru>
2025-03-28 12:00:31 +0300

    update prifile

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls.conf import path
 
 from users.apps import UsersConfig
-from users.views import LoginAPIView
+from users.views import LoginAPIView, ProfileUpdateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('profile/update/', ProfileUpdateAPIView.as_view(), name='profile_update'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
+from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class LoginAPIView(TokenObtainPairView):
     """Login."""
+
+
+class ProfileUpdateAPIView(generics.UpdateAPIView):
+    """Update profile."""
